34b0f6d82dd679873349c11904c8a6be9d38a9b5
AdamSanch <adamsan1234@gmail.com>
2023-02-28 14:20:27 -0500

    Added student, TA, and instructor dirived classes.

---
+++ Lib.LearningManagementSys/People/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Lib.LearningManagementSys.People
+{
+	public class Student : Person
+	{
+		public Student()
+		{
+            Grades = String.Empty;
+        }
+
+        public PersonClassification Classification { get; set; }
+
+        public string Grades { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Classification}";
+        }
+    }
+
+    public enum PersonClassification
+    {
+        Freshman, Sophmore, Junior, Senior
+    }
+}
+
