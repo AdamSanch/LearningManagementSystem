8da2b1f7b22626d6c84afa82fa41930b7c02ef5e
AdamSanch <adamsan1234@gmail.com>
2023-03-03 15:43:37 -0500

    -Added credit hours -cleaned AddSubmission

--- Lib.LearningManagementSys/Item/Course.cs
+++ Lib.LearningManagementSys/Item/Course.cs
@@ -10,18 +10,20 @@ namespace Lib.LearningManagementSys.Item
 
         public string Description { get; set; }
 
+        public int CreditHours { get; set; }
+
         public List<Person> Roster { get; set; }
 
         public List<AssignmentGroup> AssignmentGroups { get; set; }
 
         public List<Module> Modules { get; set; }
 
-
         public Course()
         {
             Code = string.Empty;
             Name = string.Empty;
             Description = string.Empty;
+            CreditHours = 0;
             Roster = new List<Person>();
             AssignmentGroups = new List<AssignmentGroup>();
             Modules = new List<Module>();
