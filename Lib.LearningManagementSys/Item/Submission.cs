3ba11acad6897c915e0db04856c2c0f881bad76d
AdamSanch <adamsan1234@gmail.com>
2023-03-03 04:04:34 -0500

    - Added ability to add subimissions for assignments

--- Lib.LearningManagementSys/Item/Submission.cs
+++ Lib.LearningManagementSys/Item/Submission.cs
@@ -5,16 +5,16 @@ namespace Lib.LearningManagementSys.Item
 {
 	public class Submission
 	{
-		public Assignment Assignment { get; set; }
+		//public Assignment Assignment { get; set; }
 
-		public int Grade { get; set; }
+		public double Grade { get; set; }
 
 		public Student Student { get; set; }
 
         public Submission()
 		{
 			Grade = 0;
-			Assignment = new Assignment();
+			//Assignment = new Assignment();
 			Student = new Student();
 		}
 	}
