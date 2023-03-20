37cde1655d2f6e8e1e34b0dec6ac56be8afc91d8
AdamSanch <adamsan1234@gmail.com>
2023-03-19 23:24:27 -0400

    - Added module item Update & Delete - Added Announcemt CRUD for course

---
+++ Lib.LearningManagementSys/Item/Announcement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Lib.LearningManagementSys.Item
+{
+	public class Announcement
+	{
+		public string Name { get; set; }
+		public string Content { get; set; }
+		public Announcement()
+		{
+			Name = string.Empty;
+			Content = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"----\n*{Name}*\n{Content}\n----";
+        }
+    }
+}
+
