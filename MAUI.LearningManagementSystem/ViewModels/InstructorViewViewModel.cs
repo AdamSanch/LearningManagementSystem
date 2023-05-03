e1fea0e283beedf4e02dbc2bb16bae8c6258339a
AdamSanch <adamsan1234@gmail.com>
2023-05-03 00:08:54 -0400

    print enrolled students

--- MAUI.LearningManagementSystem/ViewModels/InstructorViewViewModel.cs
+++ MAUI.LearningManagementSystem/ViewModels/InstructorViewViewModel.cs
@@ -1,9 +1,41 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Lib.LearningManagementSys.People;
+using Lib.LearningManagementSys.Services;
+
 namespace MAUI.LearningManagementSystem.ViewModels
 {
-	public class InstructorViewViewModel
+	public class InstructorViewViewModel: INotifyPropertyChanged
 	{
-		public InstructorViewViewModel()
+
+        public ObservableCollection<Person> People
+        {
+            get
+            {
+                return new ObservableCollection<Person>(StudentService.Current.Students);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void RefreshView()
+		{
+            NotifyPropertyChanged(nameof(People));
+		}
+
+        public void AddClick(Shell s)
+        {
+            s.GoToAsync("//PersonDetail");
+        }
+
+        public InstructorViewViewModel()
 		{
 		}
 	}
